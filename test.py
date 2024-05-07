b787e7113684fbe8b7677eafaa14cb8435512ea8
Daniel Hu <dh37@princeton.edu>
2024-05-07 18:26:07 -0400

    all

---
+++ test.py
@@ -0,0 +1,16 @@
+import torch
+import torch.distributed as dist
+
+torch.distributed.init_process_group(
+    backend="gloo",
+    init_method="tcp://10.10.1.1:9002",
+    world_size=4,
+    rank=3,
+)
+
+rank = dist.get_rank()
+group = dist.group.WORLD
+if rank == 0:
+    print("Root")
+else:
+    print("Worker")
