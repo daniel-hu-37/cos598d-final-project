81b4836506831e8f6348c03527b7e6bd970ea0f1
Daniel Hu <dh37@princeton.edu>
2024-05-07 20:05:55 -0400

    aa

--- run_glue_skeleton.py
+++ run_glue_skeleton.py
@@ -633,10 +633,12 @@ def main():
     # TODO(cos598d): load the model using from_pretrained. Remember to pass in `config` as an argument.
     # If you pass in args.model_name_or_path (e.g. "bert-base-cased"), the model weights file will be downloaded from HuggingFace.
     if config.n_token == -1:
-        print()
-        print("AFASFASJ:LKJL:J")
-        print()
         config.n_token = config.vocab_size
+    print()
+    print()
+    print(config)
+    print()
+    print()
 
     model = model_class.from_pretrained(args.model_name_or_path, config=config)
 
