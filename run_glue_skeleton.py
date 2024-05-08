2be61c2a2091720c9725e8a69d0b4399039ccdc9
Daniel Hu <dh37@princeton.edu>
2024-05-07 20:09:11 -0400

    help

--- run_glue_skeleton.py
+++ run_glue_skeleton.py
@@ -633,19 +633,22 @@ def main():
     # TODO(cos598d): load the model using from_pretrained. Remember to pass in `config` as an argument.
     # If you pass in args.model_name_or_path (e.g. "bert-base-cased"), the model weights file will be downloaded from HuggingFace.
 
-    print()
-    print()
-    print(config.n_token)
-    print()
-    print()
+    # print()
+    # print()
+    # print(config.n_token)
+    # print()
+    # print()
     if config.n_token == -1:
-        config.n_token = config.vocab_size
-    print()
-    print()
-    print("Changed?")
-    print(config.n_token)
-    print()
-    print()
+        # config.n_token = config.vocab_size
+        config = XLNetConfig.from_pretrained(
+            "xlnet-base-cased", vocab_size=config.vocab_size, n_token=config.vocab_size
+        )
+    # print()
+    # print()
+    # print("Changed?")
+    # print(config.n_token)
+    # print()
+    # print()
 
     model = model_class.from_pretrained(args.model_name_or_path, config=config)
 
